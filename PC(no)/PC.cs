a3244bdae94e93640ac5dda250b09bfb491feaca
Serhii <mywaifuqueen@gmail.com>
2023-03-27 09:39:04 +0100

    Add project files.

---
+++ PC(no)/PC.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_no_
+{
+    class VideoCard
+    {
+        public void Start() => Console.WriteLine("VideoCard starts up");
+        public void CheckMonitorConnection() => Console.WriteLine("VideoCard checked monitor connection");
+        public void RAMInfo() => Console.WriteLine("VideoCard prints RAM info");
+        public void DiscReaderInfo() => Console.WriteLine("Disc Reader Info");
+        public void HDDInfo() => Console.WriteLine("VideoCard prints HDD info");
+
+        //OFF
+        public void ShowFarewellMessage() => Console.WriteLine("Monitor shows: Farewell, user!");
+
+    }
+    class RAM
+    {
+        public void Start() => Console.WriteLine("RAM starts up");
+        public void Analyze() => Console.WriteLine("RAM analyzes something...");
+
+        //OFF
+        public void ClearRAM() => Console.WriteLine("Clearing RAM...");
+    }
+
+    class HDD
+    {
+        public void Start() => Console.WriteLine("HDD starts up");
+        public void Analyze() => Console.WriteLine("HDD analyzes itself..");
+
+        //OFF
+        public void Stop() => Console.WriteLine("HDD stops");
+    }
+
+    class DiscReader
+    {
+        public void Start() => Console.WriteLine("Disc Reader starts up");
+        public void CheckDisk() => Console.WriteLine("Disc Reader checks disk");
+    }
+
+    class PU
+    {
+        public void ApplyPower() => Console.WriteLine("PU applies power");
+        public void ApplyPowerOnVideocard() => Console.WriteLine("Pu applies power on videocard");
+        public void ApplyPowerOnRAM() => Console.WriteLine("Pu applies power on RAM");
+        public void ApplyPowerOnDiscReader() => Console.WriteLine("Pu applies power on Disk Reader");
+        public void ApplyPowerOnDiscHDD() => Console.WriteLine("Pu applies power on HDD");
+
+        //OFF
+
+        public void CutVideocardPower() => Console.WriteLine("PU cuts VideoCard power");
+        public void CutRAMPower() => Console.WriteLine("PU cuts RAM power");
+        public void CutDiskReaderPower() => Console.WriteLine("PU cuts Disk Reader power");
+        public void CutHDDPower() => Console.WriteLine("PU cuts HDD power");
+        public void Stop() => Console.WriteLine("PU turns OFF");
+    }
+    
+    class Sensor
+    {
+        public void CheckVoltage() => Console.WriteLine("Sensors check voltage");
+        public void CheckTemperature() => Console.WriteLine("Sensosrs check temperature");
+        public void CheckVideoCardTemperature() => Console.WriteLine("Sensors check Video Card temperature");
+        public void CheckRAMTemperature() => Console.WriteLine("Sensors check RAM temperature");
+        public void CheckAllSystemTemperature() => Console.WriteLine("Sensors check All system temperature");
+
+    }
+}
---
+++ PC(no)/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_no_
+{
+    class Facade
+    {
+        VideoCard videocard { get; set; }
+        RAM RAM { get; set; }
+        HDD HDD { get; set; }
+        DiscReader discReader { get; set; }
+        PU PU { get; set; }
+        Sensor sensors { get; set; }
+        public Facade(VideoCard videocard, RAM rAM, HDD hDD, DiscReader discReader, PU pU, Sensor sensors)
+        {
+            this.videocard = videocard;
+            RAM = rAM;
+            HDD = hDD;
+            this.discReader = discReader;
+            PU = pU;
+            this.sensors = sensors;
+        }
+
+        public void PC_ON()
+        {
+            PU.ApplyPower();
+            sensors.CheckVoltage();
+            sensors.CheckTemperature();
+            sensors.CheckVideoCardTemperature();
+            PU.ApplyPowerOnVideocard();
+            videocard.Start();
+            videocard.CheckMonitorConnection();
+            sensors.CheckRAMTemperature();
+            PU.ApplyPowerOnRAM();
+            RAM.Start();
+            RAM.Analyze();
+            videocard.RAMInfo();
+            PU.ApplyPowerOnDiscReader();
+            discReader.Start();
+            discReader.CheckDisk();
+            videocard.DiscReaderInfo();
+            PU.ApplyPowerOnDiscHDD();
+            HDD.Start();
+            HDD.Analyze();
+            videocard.HDDInfo();
+            sensors.CheckAllSystemTemperature();
+            Console.WriteLine("PC is turned on now!");
+        }
+        
+        public void PC_OFF()
+        {
+            HDD.Stop();
+            RAM.ClearRAM();
+            RAM.Analyze();
+            videocard.ShowFarewellMessage();
+            PU.CutVideocardPower();
+            PU.CutRAMPower();
+            PU.CutDiskReaderPower();
+            PU.CutHDDPower();
+            sensors.CheckVoltage();
+            PU.Stop();
+        }
+
+
+
+        //ON OFF
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoCard videoCard = new VideoCard();
+            RAM RAM = new RAM();
+            HDD HDD = new HDD();
+            DiscReader discReader = new DiscReader();
+            PU PU = new PU();
+            Sensor sensor = new Sensor();
+
+            Facade PC = new Facade(videoCard,RAM,HDD,discReader,PU,sensor);
+
+            PC.PC_ON();
+            Console.WriteLine();
+            PC.PC_OFF();
+        }
+    }
+}
